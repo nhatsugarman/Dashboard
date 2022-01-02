62f5e7263338dc3583f5a15a5508ddcf28871b21
unknown <you@example.com>
2022-01-02 10:21:04 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { createStore } from 'redux';
+
+import { Provider } from 'react-redux';
+
+import rootReducer from './redux/reducers';
+
+import './assets/boxicons/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/index.css'
+import './assets/css/theme.css'
+
+import Layout from './components/Layout/Layout'
+
+const store = createStore(
+  rootReducer
+)
+
+document.title = 'tua'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
